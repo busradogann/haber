5e4de76b131c2b1446dd26c4e2aa236bb1af7592
busradogann <busradogan1993@gmail.com>
2019-01-25 16:12:23 +0300

    deneme

--- app/views.py
+++ app/views.py
@@ -5,5 +5,5 @@ from .models import Haber
 
 
 def haber(request):
-	haber = Haber.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
-	return render(request, 'haber.html', {'haber':haber})
+	haberler = Haber.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+	return render(request, 'haber.html', {'haberler':haberler})
