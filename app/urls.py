9c2e63e865e41325dde4003dbe4615656972ff90
busradogann <busradogan1993@gmail.com>
2019-01-28 02:38:03 +0300

    form eklendi

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.haber, name='haber'),
+    path('signup' , views . SignUpView . as_view (), name = 'signup' ), 
+	path('ajax/validate_username/' , views . validate_username , name = 'validate_username' ),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Haber
+from django.contrib.auth.forms import UserCreationForm 
+from django.views.generic.edit import CreateView 
+from django.contrib.auth.models import User 
+from django.http import JsonResponse 
+
+def validate_username (request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		data = {
+			'status': 200,
+			'is_taken': User.objects.filter(username__iexact=username).exists(),}
+	return JsonResponse(data)
+
+
+class SignUpView ( CreateView ): 
+	template_name = 'signup.html' 
+	form_class = UserCreationForm
+
 
 
 
