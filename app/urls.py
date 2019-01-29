d729932f1c0f1414657da912e27775d6c69c658e
busradogann <busradogan1993@gmail.com>
2019-01-30 02:44:03 +0300

    Detail view' i ile veriler Json formatinda dönüldü.

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.haber, name='haber'),
+	path('<int:pk>/', views.detail, name='detail'),
     path('signup' , views . SignUpView . as_view (), name = 'signup' ), 
 	path('ajax/validate_username/' , views . validate_username , name = 'validate_username' ),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Haber
 from django.contrib.auth.forms import UserCreationForm 
 from django.views.generic.edit import CreateView 
 from django.contrib.auth.models import User 
 from django.http import JsonResponse 
+
 
 def validate_username (request):
 	if request.method == 'POST':
@@ -20,8 +21,47 @@ class SignUpView ( CreateView ):
 	form_class = UserCreationForm
 
 
-
-
 def haber(request):
 	haberler = Haber.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
 	return render(request, 'haber.html', {'haberler':haberler})
+
+
+def detail(request, pk):
+	haber = get_object_or_404(Haber, pk=pk)
+	data = {
+		'status' : 200,
+		'baslik' : haber.baslik,
+		'icerik' : haber.icerik,
+		'yayinlanma_tarihi' : haber.yayinlanma_tarihi,
+		}
+	return JsonResponse(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
